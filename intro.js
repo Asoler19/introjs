6eff459811934640bdd0130d84b0ec3660c80149
asoler91 <32654790+Asoler19@users.noreply.github.com>
2017-10-19 17:06:13 -0700

    updated

--- intro.js
+++ intro.js
@@ -1,14 +1,13 @@
-// Exercise 1 
-console.log("\n");
+// Exercise 1
 var op1 = 22 + 2;
 var op2 = 26 - 2;
 var op3 = 12 * 2;
 var op4 = 48 / 2;
 
-console.log(ope1);
-console.log(ope2);
-console.log(ope3);
-console.log(ope4);
+console.log(op1);
+console.log(op2);
+console.log(op3);
+console.log(op4);
 
 //  Exercise 2
 for(var i=0; i<10; i++){
@@ -16,15 +15,20 @@ for(var i=0; i<10; i++){
     }
 }
 
-var ope1 = 6% 3;
-var ope2 = 10% 2;
-var ope3 = 5% 3;
+var ope1 = 6%3;
+var ope2 = 10%2;
+var ope3 = 5%3;
 
 console.log(ope1 !== ope2);
-
 console.log(ope1 == ope2);
-console.log("Test Grad=es as follows \n"  + "\t Student1 :96 \n" + "\t Comment: \"None\"");
 
+// Exercise 3
+var line1 = "Test Grades as follows:";
+var line2 = "\n \t Student 1:96";
+var line3 = "\n \t Comment: \"None\"";
+console.log(line1 + line2 + line3);
+
+// Exercise 5
 var student ={
     name: "Alex",
     dob: '11/12/91',
@@ -34,13 +38,13 @@ var student ={
 }
 console.log(student['name'] + "\n" + student.dob);
 
+// Exercise 6
 var num = 2;
 while(num < 10){
     console.log(num);
     num+=2;
 }
-console.log("Exercise 7 \n")
-
+//  Exercise 7
 for(num = 1; num<10; num+=2){
     console.log(num);
 }
--- js/script.js
+++ js/script.js
@@ -7,8 +7,92 @@ var user ={
 
 }
 
+var invoices = [
+    {
+        inv_id: "0156321",
+        companyName: user.companyName,
+        date: "9/7/2017",
+        order: "Dell Computers",
+        amount: 3,
+        cost: 3457.67,
+        paid: 3457.67,
+    },
+    {
+        inv_id: "0145632",
+        companyName: user.companyName,
+        date: "1/5/2017",
+        order: "HP Printers",
+        amount: 2,
+        cost: 248.00,
+        paid: 150.00,
+    },
+    {
+        inv_id: "0132110",
+        companyName: user.companyName,
+        date: "6/14/2016",
+        order: "Marketing Coffee Mugs",
+        amount: 160,
+        cost: 414.00,
+        paid: 414.00,
+    },
+    {
+        inv_id: "0134560",
+        companyName: user.companyName,
+        date: "4/10/2016",
+        order: "AC Fans",
+        amount: 10,
+        cost: 200.00,
+        paid: 200.00,
+    },
+    {
+        inv_id: "5121310",
+        companyName: user.companyName,
+        date: "3/14/2016",
+        order: "Video Projectors",
+        amount: 180,
+        cost: 500.00,
+        paid: 500.00,
+    },
+    {
+        inv_id: "0737170",
+        companyName: user.companyName,
+        date: "1/09/2016",
+        order: "Chairs",
+        amount: 100,
+        cost: 300.00,
+        paid: 300.00,
+    }
+]
+
+   
+
 document.querySelector('.name').textContent = user.name;
 document.querySelector('.company_name').textContent = user.companyName;
 document.querySelector('.address').textContent = user.address;
 document.querySelector('.email').textContent = user.email;
 
+//  adding invoices
+for (var i=0; i<invoices.length; i++){
+    document.querySelector('.invoiceContainer').innerHTML += `
+    <div class="invoice" id=${invoices[i].inv_id}>
+        Invoice #:  <br />
+        <p>${invoices[i].inv_id} </p>
+        Company Name:   <br />
+        <p>${invoices[i].companyName} </p>
+        Date:  <br />
+        <p>${invoices[i].date} </p>
+        Order:   <br />
+        <p>${invoices[i].order} </p>
+        Amount:   <br />
+        <p>${invoices[i].amount} </p>
+        Cost:   <br />
+        <p>$${invoices[i].cost} </p>
+        Paid:   <br />
+        <p>$${invoices[i].paid} </p>
+        Owed:   <br />
+        <p>$${invoices[i].cost - invoices[i].paid} </p>
+        Paid:   <br />
+        <p>$${invoices[i].paid} </p>
+    </div> 
+    `   
+}
